38e36cc1c26372e0453a737f5c3e9ac7d6f1746e
LlamameAsh <JossCuriel117@gmail.com>
2020-11-28 17:58:22 -0600

    Mejora PruebaFigurasGeometricas

--- src/com/poo/practica8/PruebaFigurasGeometricas.java
+++ src/com/poo/practica8/PruebaFigurasGeometricas.java
@@ -20,7 +20,7 @@ public class PruebaFigurasGeometricas {
          * Triangulo, sin embargo, los métodos propios de cada subclase quedan aún desconocidos.
          */
         Cuadrilatero cuadrado2 = new Cuadrado();
-        System.out.println("\n\nSoy un "+cuadrado2.toString());
+        System.out.println("\nSoy un "+cuadrado2.toString());
 
         cuadrado2.setLado(5);
         cuadrado2.setAltura(5);
@@ -32,6 +32,28 @@ public class PruebaFigurasGeometricas {
 
         //cuadrado2.getAngulos(); Método contenido en la clase Cuadrado, pero no en la clase Cuadrilatero.
 
+        /**Creación de referencias a partir de los objetos Cuadrado, Rombo y Triangulo Escaleno. De esta manera, ya tenemos acceso a cada metodo
+         * heredado y también definido dentro de cada clase.
+         */
+        System.out.println("\n\nSe crea un triangulo cuyo lado a sea igual a 15[cm] y los angulos A,B y C sean 46,74 y 60 grados respectivamente");
+        TrianguloEscaleno triangulo = new TrianguloEscaleno();
+        System.out.println("Soy un "+triangulo.toString());
 
+        /**Inicialización de variables para la prueba*/
+        triangulo.setLadoA(15);
+        triangulo.setAlpha(46);
+        triangulo.setBeta(74);
+        triangulo.setGamma(60);
+
+        System.out.println("Se utiliza la funcion leySeno para obtener el lado B");
+        triangulo.setLadoB(triangulo.leySeno(triangulo.getLadoA(), triangulo.getAlpha(), triangulo.getBeta()));
+        System.out.println("Se vuelve a usar la funcion para obtener el lado C");
+        triangulo.setLadoC(triangulo.leySeno(triangulo.getLadoA(), triangulo.getAlpha(), triangulo.getGamma()));
+
+        System.out.println("Obtenemos el valor de los angulos dados en grado, ahora en radianes.");
+        triangulo.ConvAngARad();
+
+        System.out.println("Imprimimos toda la información del Triangulo para verificar que se pueden manipular los valores de manera correcta.");
+        triangulo.imprimirDatos();
     }
 }
