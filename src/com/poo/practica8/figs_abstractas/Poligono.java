353be8de885a404f6ac219f96652bbf00a928fd2
LlamameAsh <JossCuriel117@gmail.com>
2020-11-28 20:08:11 -0600

    Comentarios de documentación y pruebas realizadas. Nota: El método leySeno regresa valores incorrectos.

--- src/com/poo/practica8/figs_abstractas/Poligono.java
+++ src/com/poo/practica8/figs_abstractas/Poligono.java
@@ -1,7 +1,9 @@
 package com.poo.practica8.figs_abstractas;
 
+/**Multiherencia de las interfaces para la conversion de los angulos*/
 public abstract class Poligono implements AngulosARadianes,RadianesAAngulos{
 
+    /**Declaración de todos los métodos que contendrá la clase Poligono y por ende todas sus subclases.*/
     public abstract double area();
 
     public abstract double perimetro();
@@ -10,6 +12,7 @@ public abstract class Poligono implements AngulosARadianes,RadianesAAngulos{
 
     public abstract void ConvRadAAng();
 
+    /**Sobreescritua del método toString.*/
     public String toString() {
         return "Poligono";
     }
